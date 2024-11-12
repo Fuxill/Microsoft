22293d07b323d2b31190f94ce2037fdbf496a171
Fuxill <59236594+Fuxill@users.noreply.github.com>
2024-11-12 21:35:57 +0100

    Add files via upload

---
+++ MG_Invocation.ps1
@@ -0,0 +1,15 @@
+﻿for($mg="mg en cours d'invocation...";$mg -eq "mg en cours d'invocation..." )
+
+{
+
+$mg="mg en cours d'invocation..."
+
+[reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+[reflection.assembly]::loadwithpartialname('System.Drawing')
+$notify = new-object system.windows.forms.notifyicon
+$notify.icon = [System.Drawing.SystemIcons]::error
+$notify.visible = $true
+$notify.showballoontip(10,'Attention',$mg,[system.windows.forms.tooltipicon]::None)
+}
+
+return finito
