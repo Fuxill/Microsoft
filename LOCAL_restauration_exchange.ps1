bd610c6d817f38b7892aef08d174226029bd169f
Fuxill <59236594+Fuxill@users.noreply.github.com>
2025-05-25 23:09:16 +0200

    PS-RESTAURATION-EXCHPREM

---
+++ LOCAL_restauration_exchange.ps1
@@ -0,0 +1,16 @@
+﻿#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Script à exécuter en local sur le serveur exchange !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+# Script de redémarage des services exchange de base en cas d'arret ou disparition de ceux si de la console MMC "services" #
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Script à exécuter en local sur le serveur exchange !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
+
+    # Arrête les services de base d'exchange
+
+    Get-service -name "msexchange*" -dependentservices | Where-Object {$_.Status -eq 'Running'} | Stop-Service
+
+    # Démarre les services de base d'exchange
+
+    Get-service -name "msexchange*" -dependentservices | Where-Object {$_.Status -eq 'stopped'} | Start-Service
+
+    # Notifie dans un fichier de log la derniere utilisation du script
+
+    $date =(Get-Date)
+    echo Derniere utilisation du script de restauration des service exchange le $date > incident_exchange.log
